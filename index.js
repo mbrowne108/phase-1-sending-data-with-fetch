9562dfbd4f72552d5b7c51c3655f2d1eab098678
mbrowne108 <mbrowne108@gmail.com>
2021-09-28 14:51:55 -0400

    commit tree 1e88e0039e30fcd43cb676064fa490b10afa5941

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const userData = {
+    name: 'Steve',
+    email: 'steve@steve.com'
+}
+
+const configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(userData)
+}
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', configObj)
+        .then(res => res.json())
+        .then(data => document.body.innerHTML = data.id)
+        .catch(function (error) {
+            alert("Unauthorized Access");
+            document.body.innerHTML = error.message;
+          })
+}
